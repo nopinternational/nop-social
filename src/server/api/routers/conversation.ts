114b49878ce1a7bb7be2bd18e3db0141133544b1
Emil Cardell <emil.cardell@gmail.com>
2023-03-27 23:01:04 +0200

    Added simple message hydrtion

--- src/server/api/routers/conversation.ts
+++ src/server/api/routers/conversation.ts
@@ -1,5 +1,5 @@
 import { Message } from "@prisma/client";
-import { observable } from '@trpc/server/observable';
+import { observable } from "@trpc/server/observable";
 import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "../trpc";
@@ -13,7 +13,9 @@ export const conversationRouter = createTRPCRouter({
 
   getMyConversations: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.conversation.findMany({
-      where: { participants: { some: { userId: ctx.session.user.name as string } }},
+      where: {
+        participants: { some: { userId: ctx.session.user.name as string } },
+      },
       include: { participants: true },
     });
   }),
@@ -71,51 +73,63 @@ export const conversationRouter = createTRPCRouter({
       return newParticipation;
     }),
 
-    sendMessage: protectedProcedure
+  sendMessage: protectedProcedure
     .input(z.object({ conversationId: z.string(), content: z.string() }))
     .mutation(async ({ ctx, input }) => {
-
       const conversation = await ctx.prisma.conversation.findUnique({
         where: { id: input.conversationId },
         include: { participants: true },
       });
 
-      if(!conversation) { 
+      if (!conversation) {
         throw new Error("Conversation not found");
       }
 
-      if(!conversation.participants.find(participant => participant.userId === ctx.session.user.name)) { 
+      if (
+        !conversation.participants.find(
+          (participant) => participant.userId === ctx.session.user.name
+        )
+      ) {
         throw new Error("You are not part of conversation");
-      }     
+      }
 
-      const message = await ctx.prisma.message.create({ 
+      const message = await ctx.prisma.message.create({
         data: {
           conversationId: input.conversationId,
           content: input.content,
           userId: ctx.session.user.name as string,
-        }
+        },
       });
 
-      ctx.eventEmitter.emit('message' + input.conversationId, message);
+      ctx.eventEmitter.emit("message" + input.conversationId, message);
 
       return message;
     }),
-    onSendMessage: protectedProcedure
+  onSendMessage: protectedProcedure
     .input(z.object({ conversationId: z.string() }))
-    .subscription(({ctx, input}) => {
+    .subscription(({ ctx, input }) => {
       // return an `observable` with a callback which is triggered immediately
       return observable<Message>((emit) => {
         const onAdd = (data: Message) => {
           // emit data to client
-          if(input.conversationId === data.conversationId){
+          if (input.conversationId === data.conversationId) {
             emit.next(data);
-          }          
+          }
         };
+        void ctx.prisma.message
+          .findMany({
+            where: { conversationId: input.conversationId },
+            orderBy: { createdAt: "asc" },
+          })
+          .then((messages) => messages.forEach((message) => onAdd(message)))
+          .then(() =>
+            ctx.eventEmitter.on("message" + input.conversationId, onAdd)
+          );
         // trigger `onAdd()` when `add` is triggered in our event emitter
-        ctx.eventEmitter.on('message' + input.conversationId, onAdd);
+
         // unsubscribe function when client disconnects or stops subscribing
         return () => {
-          ctx.eventEmitter.off('message' + input.conversationId, onAdd);
+          ctx.eventEmitter.off("message" + input.conversationId, onAdd);
         };
       });
     }),
