551fda562564f73a5d9665a063ff8b0ad99756e3
nopops <nopops@nightofpassion.se>
2023-03-30 22:02:59 +0200

    a little more of styling

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,22 +1,33 @@
 
-export function UserList({usernames}:{usernames:string[]}) {
-return <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-    <div className="flex items-center justify-between mb-4">
-        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Participants</h5>
-   </div>
-   <div className="flow-root">
-        <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+export function UserList({ usernames }: { usernames: string[] }) {
+    return <div className="max-w shrink-0 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className=" items-center justify-between mb-4">
+            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Participants</h5>
+        </div>
+        <div className="flex flex-row flex-wrap gap-2">
+
             {usernames.map((username) => {
-            return <li key={username} className="py-3 sm:py-4">
-                <div className="flex items-center space-x-4">
-                    <div className="flex-1 min-w-0">
-                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                            {username}
-                        </p>
+                return (
+                    <div key={username} className="flex-8 space-x-4 flex-8 min-w-0">
+                        <button
+                            className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20 truncate"
+                            onClick={() => null}
+                        > {username}</button>
                     </div>
-                </div>
-            </li>})}
-        </ul>
-   </div>
-</div>
+                )
+            })}
+
+            {usernames.map((username) => {
+                return (
+                    <div key={username} className="flex-8 space-x-4 flex-8 min-w-0">
+                        <button
+                            className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20 truncate"
+                            onClick={() => null}
+                        > {username}</button>
+                    </div>
+                )
+            })}
+
+        </div>
+    </div>
 }
