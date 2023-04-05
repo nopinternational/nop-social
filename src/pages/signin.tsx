504d523da81a268ab2cbc40f67954152ded21f96
nopops <nopops@nightofpassion.se>
2023-04-05 19:03:58 +0200

    signin page header

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -32,7 +32,6 @@ function Signin({ providers }) {
 
         return (
             <>
-                <div>nop-signin</div>
                 <form className="m-2" >
                     <div className="m-2">email</div>
                     <input
@@ -59,12 +58,12 @@ function Signin({ providers }) {
             <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
                 <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
                     <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-                        Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+                        Signin to <span className="text-[hsl(280,100%,70%)]">Night of Passion</span>
                     </h1>
                     <div className="grid grid-cols-1 sm:grid-cols-1 gap-4 md:gap-8">
                         <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
 
-                            <h3 className="text-2xl font-bold">First Steps →</h3>
+                            <h3 className="text-2xl font-bold">Signin →</h3>
                             <div className="text-lg">
                                 {nopAuthSignIn(providers)}
                             </div>
