500a1ec06a31594aa0a44c9a981cd5aeaaeac63f
nopops <nopops@nightofpassion.se>
2023-07-14 19:02:15 +0200

    center headline

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -74,7 +74,7 @@ const Signin = ({ providers }: SigninPageProps) => {
             </Head>
             <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
                 <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-                    <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+                    <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] text-center">
                         Logga in till <HighlightText>Night of Passion</HighlightText>
                     </h1>
                     <div className="grid w-full xl:w-1/2 grid-cols-1 sm:grid-cols-1 gap-4 md:gap-8">
