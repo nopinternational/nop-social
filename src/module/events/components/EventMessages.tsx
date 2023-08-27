f73f519a4fec680417e942db7bc7d33bf0be69d6
nopops <nopops@nightofpassion.se>
2023-08-27 17:36:51 +0200

    events: adding dummy messages as mockup for design

--- src/module/events/components/EventMessages.tsx
+++ src/module/events/components/EventMessages.tsx
@@ -16,9 +16,30 @@ export const EventMessages = () => {
                     </div>
 
                     <div>
-                        
-                        <div className="italic text-lg whitespace-pre-wrap p-2 bg-white/10 rounded-md">
-                            Här kommer ni snart att kunna skriva meddelenaden till andra som kommer på träffen.
+                        <div><HighlightText>Skicka ett eget meddelande:</HighlightText></div>
+                        <div className="text-black text-lg whitespace-pre-wrap p-2 bg-white/10 rounded-md">
+                            <input placeholder="lämna ert medelenade här"></input>
+                            <button className="rounded-full bg-white/10 bg-[hsl(280,100%,70%)] px-10 py-3 font-semibold text-white no-underline transition hover:bg-[hsl(280,100%,70%)]">
+                                Skicka
+                            </button>
+                        </div>
+                    </div>
+                    <div>
+                        <div><HighlightText>sthlmpar08</HighlightText> säger:</div>
+                        <div className="text-lg whitespace-pre-wrap p-2 bg-white/10 rounded-md">
+                            Vi har så mycket kul att berätta om sommmarens äventyr
+                        </div>
+                    </div>
+                    <div>
+                        <div><HighlightText>foppi76</HighlightText> säger:</div>
+                        <div className="text-lg whitespace-pre-wrap p-2 bg-white/10 rounded-md">
+                            Äntligen är ni igång gång, längtar! Har ni haft det bra i sommar?
+                        </div>
+                    </div>
+                    <div>
+                        <div><HighlightText>sthlmpar08</HighlightText> säger:</div>
+                        <div className="text-lg whitespace-pre-wrap p-2 bg-white/10 rounded-md">
+                            Vi är så glada att få träffa alla igen
                         </div>
                     </div>
 
