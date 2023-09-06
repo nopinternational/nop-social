4ab4be1f71fe40453d126a200cd27f1d36b80391
nopops <nopops@nightofpassion.se>
2023-09-06 17:32:25 +0200

    profile: still working on it

--- src/pages/app/profile/edit.tsx
+++ src/pages/app/profile/edit.tsx
@@ -19,16 +19,16 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Våra <HighlightText>härliga</HighlightText> par
+            Ändra <HighlightText>er</HighlightText> profil
           </h1>
           <div className="grid grid-cols-2  sm:grid-cols-2   gap-4 md:gap-8">
             <div className="col-span-2">
               <div className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
-                <h3 className="text-2xl font-bold">Ändra er profil</h3>
+                <h3 className="text-2xl font-bold">Problem 👷👷‍♀️</h3>
                 <div className="text-lg">
                   Just nu har vi problem med att ändra er profil. Vi jobbar på det och återkommer!
                 </div>
-                
+
               </div>
             </div >
           </div>
