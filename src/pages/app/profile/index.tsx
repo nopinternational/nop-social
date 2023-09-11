b36d2db8dc7d3df2352287650e92515540b7a9fb
nopops <nopops@nightofpassion.se>
2023-09-11 18:45:04 +0200

    profile: profile listing and details is LIVE :D

--- src/pages/app/profile/index.tsx
+++ src/pages/app/profile/index.tsx
@@ -7,8 +7,76 @@ import Footer from "~/components/Footer";
 import { api } from "~/utils/api";
 import { type Profile } from "~/module/profile/profileRouter";
 import Link from "next/link";
+import couplePic from "./couple_icon_square.png"
+import Image from 'next/image'
 
 const Home: NextPage = () => {
+  //const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  const { data: sessionData } = useSession();
+
+  const profiles = api.profile.getAllProfiles.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined }
+  );
+
+  const YEAR = new Date().getFullYear()
+
+  // if (profileQuery.isLoading) {
+  //   return <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800">Loading...</div>
+  // }
+
+  // if (!profileQuery.data) {
+  //   return <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800">Loading...</div>
+  // }
+
+
+  const renderProfile = (profile: Profile) => {
+
+    const profileSlug = `profile/${profile.username}`
+    return (
+      <Link href={profileSlug} className="col-span-2 md:col-span-1">
+        <div key={profile.username} className="flex flex-col gap-4 col-span-2 md:col-span-1 items-center w-full justify-center rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
+          <div className="">
+            <Image
+              className="w-32 h-32 bg-yellow-50 rounded-full shadow  max-w-full align-middle border-4 border-[hsl(280,100%,70%)]"
+              src={couplePic}
+              alt="John Doe" />
+          </div>
+          <h3 className="text-2xl font-bold text-center"><HighlightText>{profile.username}</HighlightText></h3>
+          {/* <div className="p-2">
+            <div className="text-lg ">
+              <p>{profile.username} är ett par som heter <HighlightText>{profile.person1?.name}</HighlightText> & <HighlightText>{profile.person2.name}</HighlightText>,
+                dom är {YEAR - profile.person1?.born} och {YEAR - profile.person2?.born}år.</p>
+            </div>
+          </div> */}
+        </div>
+      </Link>
+    )
+  }
+
+  const renderLoading = () => {
+    return (
+      <div className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20 col-span-2" >
+        <h3 className="text-2xl font-bold"><HighlightText>Laddar...</HighlightText></h3>
+        <div className="text-lg">
+          <p>Den som väntar på något gott 😘 </p>
+        </div>
+
+        <div className="h-12 w-12 mb-4">
+          <div className="flex">
+            <div className="relative">
+              <div className="w-12 h-12 rounded-full absolute
+                            border-8 border-solid border-gray-200"></div>
+              <div className="w-12 h-12 rounded-full animate-spin absolute
+                            border-8 border-solid border-[hsl(280,100%,70%)] border-t-transparent shadow-md"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
   return (
     <>
       <Head>
@@ -18,7 +86,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] text-center">
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Våra <HighlightText>härliga</HighlightText> par
           </h1>
           <div className="grid grid-cols-2  sm:grid-cols-2   gap-4 md:gap-8">
@@ -26,13 +94,17 @@ const Home: NextPage = () => {
               <div className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
                 <h3 className="text-2xl font-bold">Par, par, par ❤️❤️❤️</h3>
                 <div className="text-lg">
-                  Night of Passion är fullt av trevliga par. Här kommer ni kunna se er själva och alla andra par som är medlemmar i Night of Passion. Vi kommer att släppa denna sida efter vår cocktailträff 9/9.
-                </div>
-                <div className="text-lg">
-                  Vi ses då 😀
+                  Night of Passion är fullt av trevliga par. Nedan hittar ni några av dom 😘
                 </div>
               </div>
+
             </div >
+            {profiles.isLoading ? renderLoading() : null}
+
+
+            {profiles.data?.map((profile) => renderProfile(profile))}
+
+
           </div>
           <div className="flex flex-col items-center gap-2">
             <Footer />
