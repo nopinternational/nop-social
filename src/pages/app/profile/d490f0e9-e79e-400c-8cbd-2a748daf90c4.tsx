6dd1bbaffae0fbdb64e3029f1e3dc49fa49f1da9
nopops <nopops@nightofpassion.se>
2023-09-11 18:40:56 +0200

    profile: new profile card on listing

--- src/pages/app/profile/d490f0e9-e79e-400c-8cbd-2a748daf90c4.tsx
+++ src/pages/app/profile/d490f0e9-e79e-400c-8cbd-2a748daf90c4.tsx
@@ -38,20 +38,18 @@ const Home: NextPage = () => {
       <Link href={profileSlug} className="col-span-2 md:col-span-1">
         <div key={profile.username} className="flex flex-col gap-4 col-span-2 md:col-span-1 items-center w-full justify-center rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
           <div className="">
-            <div className="">
-              <Image
-                className="w-32 h-32 bg-yellow-50 rounded-full shadow  max-w-full align-middle border-4 border-[hsl(280,100%,70%)]"
-                src={couplePic}
-                alt="John Doe" />
-            </div>
-            <h3 className="text-2xl font-bold text-center"><HighlightText>{profile.username}</HighlightText></h3>
+            <Image
+              className="w-32 h-32 bg-yellow-50 rounded-full shadow  max-w-full align-middle border-4 border-[hsl(280,100%,70%)]"
+              src={couplePic}
+              alt="John Doe" />
           </div>
-          <div className="p-2">
+          <h3 className="text-2xl font-bold text-center"><HighlightText>{profile.username}</HighlightText></h3>
+          {/* <div className="p-2">
             <div className="text-lg ">
               <p>{profile.username} är ett par som heter <HighlightText>{profile.person1?.name}</HighlightText> & <HighlightText>{profile.person2.name}</HighlightText>,
                 dom är {YEAR - profile.person1?.born} och {YEAR - profile.person2?.born}år.</p>
             </div>
-          </div>
+          </div> */}
         </div>
       </Link>
     )
