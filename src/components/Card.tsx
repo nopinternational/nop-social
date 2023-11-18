07bf7bda4ee602bc8927b79b4d4c4d7513f5f83e
nopops <nopops@nightofpassion.se>
2023-11-18 12:26:42 +0100

    events: basic form structure with inputs

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,7 @@ export type CardProps = {
 }
 export const Card = ({ header, children }: CardProps) => {
     return (
-        <div className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
+        <div className="flex flex-col gap-4 rounded-xl bg-white/10 mb-4 p-4 text-white hover:bg-white/20">
             <h3 className="text-2xl font-bold">{header}</h3>
             <div className="text-lg">
                 {children}
--- src/pages/app/event/new.tsx
+++ src/pages/app/event/new.tsx
@@ -6,6 +6,12 @@ import Layout from "~/components/Layout";
 
 const Home: NextPage = () => {
 
+
+    const createEvent = (e: React.MouseEvent<HTMLButtonElement>): void => {
+        e.preventDefault()
+        console.log("create Event", e.target)
+    }
+
     return (
         <Layout headingText={<>Ordna en <HighlightText>träff</HighlightText></>}>
             <div className="grid grid-cols-2  sm:grid-cols-2   gap-4 md:gap-8">
@@ -13,6 +19,34 @@ const Home: NextPage = () => {
                     <Card header={<>Ordna <HighlightText>träff</HighlightText></>}>
                         Skapa en träff och samla alla sexiga par i <HighlightText>Night of Passion</HighlightText>
                     </Card>
+
+                    <Card header="Detaljer">
+                        <form className="p-2" >
+                            <div className="m-2">Titel</div>
+                            <input
+                                className="w-full px-3 py-3 rounded-lg text-black"
+                                name="title" value="titel..." /><br />
+
+                            <div className="m-2">När</div>
+                            <input className="w-full px-3 py-3 rounded-lg text-black"
+                                name="when" value="när..." /><br />
+
+                            <div className="m-2">Kort beskrivning</div>
+                            <textarea className="w-full px-3 py-3 rounded-lg text-black"
+                                name="shortDesc" >kort beskrivning</textarea>
+
+                            <div className="m-2">Lång beskrivning</div>
+                            <textarea className="w-full px-3 py-3 rounded-lg text-black"
+                                name="longDesc" >lång beskrivning</textarea>
+
+                            <div className="m-2">Options...</div>
+
+                            <button
+                                className="mt-4 mb-3 rounded-full bg-[hsl(280,100%,70%)] px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+                                onClick={(event: React.MouseEvent<HTMLButtonElement>) => createEvent(event)}>Ändra</button>
+                        </form>
+                    </Card>
+
 
                 </div>
             </div>
