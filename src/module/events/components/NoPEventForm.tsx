bd15c5d1aade5cf113196f9b8be47bdbcae5e7ec
nopops <nopops@nightofpassion.se>
2023-11-24 08:40:52 +0100

    event: toggle pixel pushing

--- src/module/events/components/NoPEventForm.tsx
+++ src/module/events/components/NoPEventForm.tsx
@@ -185,7 +185,24 @@ const Toggle = ({ name, optionText, checked, onClick }: { name: string, optionTe
                 onClick={onClick}
             />
             <div
-                className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"
+                className="w-11
+                h-6
+                bg-gray-200
+                rounded-full peer
+                peer-focus:ring-green-300
+                peer-checked:after:translate-x-full
+                peer-checked:after:border-white after:content-['']
+                after:absolute after:top-0.5
+                after:left-[2px]
+                after:bg-white
+                after:border-gray-300
+                after:border
+                after:rounded-full
+                after:h-5
+                after:w-5
+                after:transition-all
+                after:mt-0.5
+                peer-checked:bg-green-600"
             ></div>
             <span className=" ">
                 {optionText}
