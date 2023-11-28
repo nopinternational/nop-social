205778551c845fcb7cf853838797c333d208bf28
nopops <nopops@nightofpassion.se>
2023-11-28 21:06:14 +0100

    messages: styling touch up

--- src/pages/app/message/index.tsx
+++ src/pages/app/message/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
             <div className="grid grid-cols-2  sm:grid-cols-2   gap-4 md:gap-8">
                 <div className="col-span-2">
                     <div className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
-                        <h3 className="text-2xl font-bold">Skicka meddelande till andra profiler</h3>
+                        <h3 className="text-2xl font-bold">Skicka <HighlightText>meddelande</HighlightText> till andra <HighlightText>profiler</HighlightText></h3>
                         <div className="text-lg">
                             Tjoho! Just nu arbetar vi med att göra det möjligt att skicka meddelande till varandra. Bra va 😃
                         </div>
