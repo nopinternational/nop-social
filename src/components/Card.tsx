70dc9597d7530e62d3bc61a175d9d2d73ae62daf
nopops <nopops@nightofpassion.se>
2023-11-29 16:47:51 +0100

    card: children jox

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,11 +7,9 @@ export type CardProps = {
 }
 export const Card = ({ header, children }: CardProps) => {
     return (
-        <div className="flex flex-col gap-4 rounded-xl bg-white/10 mb-4 p-4 text-white hover:bg-white/20">
+        <div className="flex flex-col gap-4 rounded-xl text-lg bg-white/10 mb-4 p-4 text-white hover:bg-white/20">
             <h3 className="text-2xl font-bold">{header}</h3>
-            <div className="text-lg">
-                {children}
-            </div>
+            {children}
         </div>
 
     )
