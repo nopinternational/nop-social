cf39d52a6ccb2d963cdb0abbda7af8a224eb699c
nopops <nopops@nightofpassion.se>
2023-11-30 22:03:38 +0100

    message: layout on message listing

--- src/module/profile/components/ProfileHeader.tsx
+++ src/module/profile/components/ProfileHeader.tsx
@@ -4,12 +4,12 @@ import { ProfilePic } from "./ProfilePic";
 export const ProfileHeader = ({ profileName }: { profileName: string }) => {
 
     return (
-        <div className="">
+        <>
             <div className="mb-4">
                 <ProfilePic />
             </div>
             <h3 className="text-2xl font-bold text-center"><HighlightText>{profileName}</HighlightText></h3>
-        </div>
+        </>
     )
 }
 
