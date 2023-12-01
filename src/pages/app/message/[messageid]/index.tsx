1fe42e02d794ad4e1b6a5aab7dd36c96201c0b20
nopops <nopops@nightofpassion.se>
2023-12-01 17:41:05 +0100

    message: linking to message

--- src/pages/app/message/[messageid]/index.tsx
+++ src/pages/app/message/[messageid]/index.tsx
@@ -8,7 +8,6 @@ import { ProfilePic } from "~/module/profile/components/ProfilePic";
 
 
 type Conversation = {
-    conversationId: string
     username: string
     lastMessage: string
 }
@@ -17,18 +16,14 @@ const Home: NextPage = () => {
 
     const CONVERSATION: Conversation[] = [
         {
-            conversationId: "e36db886ceadadf6e26678b57222a6d0",
             username: "sthlmpar08",
             lastMessage: "Så roligt att höra! Vi ses på lördag för en middag"
-
         },
         {
-            conversationId: "6f0216ac2fa4cee37679b55795f5517d",
             username: "Sexy-couple",
             lastMessage: "Det var många härliga par på förra träffen och ni är ett par som vi särskilt gillar 😉"
         },
         {
-            conversationId: "7061c9f8e194f2076a40e0b988a00859",
             username: "Latin-Language-Lovers",
             lastMessage: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
         }
@@ -49,21 +44,21 @@ const Home: NextPage = () => {
                     </Card >
 
                     <Card header="Pågående konversationer">
-
-                        {CONVERSATION.map(convo => (
-                            <>
-                                <Link
-                                    className="grid grid-cols-4 "
-                                    href={`/app/message/${convo.conversationId}`}>
-                                    <Conversation convo={convo} />
-                                </Link >
-                            </>
-                        ))}
-
+                        <p className=" p-2 rounded-xl bg-white/10 whitespace-pre-wrap italic" >
+                            Så roligt att höra! Vi ses på lördag för en middag
+                        </p>
+                        {/* <div className="grid grid-cols-4 gap-y-8">
+                        </div> */}
                     </Card>
 
                 </div >
             </div >
+            <Link href="/app/message">
+                <button
+                    className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
+                    Tillbaka till meddelanden
+                </button>
+            </Link>
         </Layout >
     )
 };
