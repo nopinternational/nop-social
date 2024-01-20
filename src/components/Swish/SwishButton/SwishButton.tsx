8324210e04c1516ef2ad4341e568aecffa749e4a
nopops <nopops@nightofpassion.se>
2024-01-20 15:57:30 +0100

    event: swish button with prefilled data

--- src/components/Swish/SwishButton/SwishButton.tsx
+++ src/components/Swish/SwishButton/SwishButton.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import Image, { type StaticImageData } from 'next/image';
+import swishLogoSVG from "./swishLogoSVG.svg"
 
 type SwishButtonProp = {
     swishto?: string
     amount: number
     message: string
     // editable?: {
-    //     swishto: boolean
     //     amt: boolean
     //     message: boolean
     // }
@@ -17,13 +18,20 @@ export const CocktailSwishButton = ({ message }: { message: string }) => {
 }
 
 export const SwishButton = ({ swishto = "+46700066099", amount, message }: SwishButtonProp) => {
-
+    // How to generate url
+    // https://developer.swish.nu/documentation/guides/generate-qr-codes
     const link = `https://app.swish.nu/1/p/sw/?sw=${swishto}&amt=${amount}&cur=SEK&src=qr&msg=${message}&edit=msg,amt`
     return (<>
         <Link href={link}>
             <button
-                className="relative rounded-full bg-green-600 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
-                Swisha
+                className="flex items-center gap-3 relative rounded-full bg-green-600 px-8 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
+                <Image
+                    priority
+                    src={swishLogoSVG as StaticImageData}
+                    alt="Follow us on Twitter"
+                    height={30}
+                />
+                <span>Swisha</span>
             </button>
         </Link>
     </>)
