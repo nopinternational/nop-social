c4a13019e34846f6de8ebfa44dbdc579fee61290
nopops <nopops@nightofpassion.se>
2024-04-24 19:11:56 +0200

    feat(user) add a code input  card for password reset

--- src/pages/password-reset/index.tsx
+++ src/pages/password-reset/index.tsx
@@ -1,5 +1,4 @@
 import { type NextPage } from "next";
-import Link from "next/link";
 import { useRef } from "react";
 import { Card } from "~/components/Card";
 import HighlightText from "~/components/HighlightText";
@@ -7,6 +6,17 @@ import Layout from "~/components/Layout";
 
 const Home: NextPage = () => {
   //const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <Layout headingText={<HighlightText>Night of Passion</HighlightText>}>
+      <EmailCard></EmailCard>
+      <CodeCard></CodeCard>
+    </Layout>
+  );
+};
+
+export default Home;
+const EmailCard = () => {
   const inputUsername = useRef<HTMLInputElement>(null);
   const submitFormClick = (
     event: React.MouseEvent<HTMLButtonElement>
@@ -19,36 +29,69 @@ const Home: NextPage = () => {
       inputUsername.current?.value.trim()
     );
   };
-
   return (
-    <Layout headingText={<HighlightText>Night of Passion</HighlightText>}>
-      <Card header="Återställ lösenord">
-        <div className="text-lg">
-          Här kan du återställa ditt lösenord. Ange din epost så kommer vi att
-          skicka ett mail till er med en återställningskod.
-        </div>
-        <form className="p-2">
-          <div className="m-2">email</div>
-          <input
-            className="w-full rounded-lg px-3 py-3 text-center text-black"
-            name="username"
-            ref={inputUsername}
-          ></input>
-          <br />
+    <Card header="Återställ lösenord">
+      <div className="text-lg">
+        Här kan du återställa ditt lösenord. Ange din epost så kommer vi att
+        skicka ett mail till er med en återställningskod.
+      </div>
+      <form className="p-2">
+        <div className="m-2">email</div>
+        <input
+          className="w-full rounded-lg px-3 py-3 text-center text-black"
+          name="username"
+          ref={inputUsername}
+        ></input>
+        <br />
 
-          <br />
-          <button
-            className="mb-3 mt-4 rounded-full bg-[hsl(280,100%,70%)] px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-            onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
-              submitFormClick(event)
-            }
-          >
-            Återställ lösenord
-          </button>
-        </form>
-      </Card>
-    </Layout>
+        <br />
+        <button
+          className="mb-3 mt-4 rounded-full bg-[hsl(280,100%,70%)] px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+          onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
+            submitFormClick(event)
+          }
+        >
+          Återställ lösenord
+        </button>
+      </form>
+    </Card>
   );
 };
+const CodeCard = () => {
+  const inputCode = useRef<HTMLInputElement>(null);
+  const submitFormClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ): void => {
+    //console.log("Signin.nopAuthSignIn.signinNopAuth.event", event)
+    event.preventDefault();
+    console.log("submitFormClick ", event);
+    console.log("reset password with code ", inputCode.current?.value.trim());
+  };
+  return (
+    <Card header="Har ni fått en kod?">
+      <div className="text-lg">
+        Har ni fått en kod så kan ni ange den nedan för att återställa
+        lösenorder
+      </div>
+      <form className="p-2">
+        <div className="m-2">Återställningskod</div>
+        <input
+          className="w-full rounded-lg px-3 py-3 text-center text-black"
+          name="username"
+          ref={inputCode}
+        ></input>
+        <br />
 
-export default Home;
+        <br />
+        <button
+          className="mb-3 mt-4 rounded-full bg-[hsl(280,100%,70%)] px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+          onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
+            submitFormClick(event)
+          }
+        >
+          Återställ lösenord
+        </button>
+      </form>
+    </Card>
+  );
+};
