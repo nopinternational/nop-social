2bd40c3a7340a7bf646c66d0eaf33cb95419756d
nopops <nopops@nightofpassion.se>
2024-08-21 20:09:07 +0200

    meesage: remove message feature toggle

--- src/components/FeatureFlag.tsx
+++ src/components/FeatureFlag.tsx
@@ -1,18 +1,19 @@
-import { useFlag, useUnleashContext } from "@unleash/nextjs/client";
+//import { useFlag, useUnleashContext } from "@unleash/nextjs/client";
 import { type ReactNode, useEffect } from "react";
 import { useSession } from "next-auth/react";
 
 export const useFeature = (featureName: string): boolean => {
     const session = useSession()
 
-    const featureIsEnabled = useFlag(featureName)
-    const updateContext = useUnleashContext();
+    //const featureIsEnabled = useFlag(featureName)
+    //const updateContext = useUnleashContext();
 
     useEffect(() => {
         const userId = session.data?.user.id
-        void updateContext({ userId });
+        //void updateContext({ userId });
     });
-    return featureIsEnabled
+    //return featureIsEnabled
+    return false
 }
 
 export const ToggledByFeatureFlag = ({ featureName, children }: { featureName: string, children: ReactNode }): React.JSX.Element | null => {
