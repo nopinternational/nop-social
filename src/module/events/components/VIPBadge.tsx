23cae8797ee4fa13178bcecc4e1fd1a3e91fcd71
nopops <nopops@nightofpassion.se>
2024-10-14 21:50:17 +0200

    events: generic Badge component

--- src/module/events/components/VIPBadge.tsx
+++
@@ -1,5 +0,0 @@
-export const VIPBadge = ({ isVIP }: { isVIP: boolean }) => {
-  const pillClass =
-    "relative -end-2 -top-2 inline-flex h-4 items-center justify-center rounded-full bg-[hsl(280,100%,70%)] p-2 text-xs font-bold text-white dark:border-gray-900";
-  return isVIP ? <div className={pillClass}>VIP</div> : null;
-};
