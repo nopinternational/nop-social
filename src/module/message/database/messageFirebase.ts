da371a7d510f6274f9b352071f21bbd59120617d
nopops <nopops@nightofpassion.se>
2024-11-12 18:55:35 +0100

    create database file

--- src/module/message/database/messageFirebase.ts
+++ src/module/message/database/messageFirebase.ts
@@ -1,6 +1,4 @@
-import { firestoreAdmin } from "~/server/api/firebaseAdmin";
 
-const firestore: FirebaseFirestore.Firestore = firestoreAdmin;
 
 import {
     type FirestoreDataConverter,
@@ -58,42 +56,6 @@ function permute(permutation: string[]) {
     return result;
 }
 
-export const getChatMessages = async (
-    messageCollection: string
-): Promise<ConversationMessage[]> => {
-    const db = new FirbaseChatMessageClient(firestore);
-    return db.getChatMessages(messageCollection);
-};
-
-export const getConvoAndMessages = async (
-    messageCollectionId: string,
-    userId: string
-): Promise<ConvoWithMessages> => {
-    const db = new FirbaseChatMessageClient(firestore);
-    return db.getConvoAndMessages(messageCollectionId, userId);
-};
-
-export const getGroups = async (
-    userId: string
-): Promise<ConversationGroup[]> => {
-    const db = new FirbaseChatMessageClient(firestore);
-    return db.getConvoGroupsWithProfiles(userId);
-};
-
-export const persistChatMessage = async (message: ConversationMessage) => {
-    const db = new FirbaseChatMessageClient(firestore);
-    return db.storeChatMessage(message);
-};
-
-export const updateConvoMarkAsRead = (convoId: string, userId: string) => {
-    const db = new FirbaseChatMessageClient(firestore);
-    db.updateConvoMarkAsRead(convoId, userId);
-};
-
-export const persistChatMessageToUser = async (message: APIMessageToUser) => {
-    const db = new FirbaseChatMessageClient(firestore);
-    return db.persistChatMessageToUser(message);
-};
 
 const CHATMESSAGE_COLLECTION = "message";
 const GROUP_COLLECTION = "group";
@@ -108,7 +70,7 @@ type CreateConversationGroup = {
   when: string;
 };
 
-class FirbaseChatMessageClient {
+export class FirbaseChatMessageClient {
     firestore: FirebaseFirestore.Firestore;
 
     constructor(firestoreApp: FirebaseFirestore.Firestore) {
