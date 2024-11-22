e4183d7fa8c61919264ac109d8506dfa0fb29ecc
nopops <nopops@nightofpassion.se>
2024-11-22 15:20:32 +0100

    remove loader

--- src/module/profile/components/ProfilePic.tsx
+++ src/module/profile/components/ProfilePic.tsx
@@ -8,7 +8,7 @@ export const ProfilePic = ({ variant, url }: ProfilePicProps) => {
     if (variant === "small")
         return (
             <Image
-                loader={url ? () => url : undefined}
+                // loader={url ? () => url : undefined}
                 priority = { true}
                 className="min-w-fill min-h-fill h-14 w-14 max-w-full rounded-full  border-4 border-[hsl(280,100%,70%)] bg-yellow-50 align-middle shadow sm:h-32 sm:w-32"
                 src={couplePic}
@@ -20,7 +20,7 @@ export const ProfilePic = ({ variant, url }: ProfilePicProps) => {
 
     return (
         <Image
-            loader={url ? () => url : undefined}
+            // loader={url ? () => url : undefined}
             priority={true}
             className="rounded-full aspect-square object-cover h-32 w-32 border-4 border-[hsl(280,100%,70%)] bg-yellow-50 align-middle shadow sm:h-32 sm:w-32"
             src={couplePic}
