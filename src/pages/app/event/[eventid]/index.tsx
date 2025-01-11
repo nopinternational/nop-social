a00413d24b1014be04173cc422ace04241a59592
nopops <nopops@nightofpassion.se>
2025-01-11 10:46:02 +0100

    event: adding isConfirmed state and reformat

--- src/pages/app/event/[eventid]/index.tsx
+++ src/pages/app/event/[eventid]/index.tsx
@@ -26,6 +26,7 @@ const Home: NextPage = () => {
     //sessionData?.user?.append("name1") = "jw"
 
     const [attendingToEvent, setAttendToEvent] = useState(false);
+    const [isConfirmed, setConfirmed] = useState(false);
 
     const queryInput = { eventId: eventid as string };
     const event = api.event.getEvent.useQuery(queryInput, {
@@ -52,7 +53,7 @@ const Home: NextPage = () => {
                 <div className="flex flex-wrap justify-center justify-self-center">
                     <div className="p-2">
                         <button className="rounded-sm bg-pink-600 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
-              Träffen stängd för anmälan
+                            Träffen stängd för anmälan
                         </button>
                     </div>
                 </div>
@@ -65,7 +66,7 @@ const Home: NextPage = () => {
             <Layout
                 headingText={
                     <>
-            Laddar <HighlightText>träff</HighlightText>...
+                        Laddar <HighlightText>träff</HighlightText>...
                     </>
                 }
             >
@@ -79,14 +80,14 @@ const Home: NextPage = () => {
             <Layout
                 headingText={
                     <>
-            Ingen <span className="text-[hsl(280,100%,70%)]">träff</span> här
+                        Ingen <span className="text-[hsl(280,100%,70%)]">träff</span> här
                     </>
                 }
             >
                 <div className="grid grid-cols-2  gap-4   sm:grid-cols-2 md:gap-8">
                     <div className="col-span-2 p-2">
                         <Card header="Det finns ingen träff här...">
-              Vi är ledsna men vi hittar inte träffen du söker 😔
+                            Vi är ledsna men vi hittar inte träffen du söker 😔
                         </Card>
                     </div>
                 </div>
@@ -101,7 +102,7 @@ const Home: NextPage = () => {
         <Layout
             headingText={
                 <>
-          Träff med <HighlightText>Night of Passion</HighlightText>
+                    Träff med <HighlightText>Night of Passion</HighlightText>
                 </>
             }
         >
@@ -115,7 +116,7 @@ const Home: NextPage = () => {
                                 onClick={() => attendToEventHandler()}
                                 className="rounded-full bg-[hsl(280,100%,70%)] px-10 py-3 font-semibold text-white no-underline transition hover:bg-opacity-75"
                             >
-                Anmäl er till träffen
+                                Anmäl er till träffen
                             </button>
                         </div>
                     </div>
@@ -125,12 +126,13 @@ const Home: NextPage = () => {
                 <div className="p-2">
                     <Link href={router.asPath + "/attendes"}>
                         <button className="rounded-full bg-[hsl(280,100%,70%)] bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
-              Vilka kommer på träffen?
+                            Vilka kommer på träffen?
                         </button>
                     </Link>
                 </div>
             ) : null}
 
+            {isConfirmed ? <IsConfirmed /> : null}
             {attendingToEvent && e.options.signupOpen ? (
                 <Attending event={e} session={session.data} />
             ) : null}
@@ -144,6 +146,31 @@ const Home: NextPage = () => {
     );
 };
 
+
+const IsConfirmed = () => {
+    return (
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:gap-8">
+            <div className="col-span-2">
+                <Card
+                    header={
+                        <>
+                            Välkommen på <HighlightText>Cocktailträff 🎉🍸🍾</HighlightText>
+                        </>
+                    }
+                >
+                    <div>
+                        Ni har betalat ooch er plats är bekräftad. Vi ses på träffen!
+                    </div>
+                    <div>
+                        Vi skickar ut mer information några dagar innan träffen.
+                    </div>
+
+                </Card>
+            </div>
+        </div>
+    );
+};
+
 const EventIsClosedForAttendes = () => {
     return (
         <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:gap-8">
@@ -151,13 +178,13 @@ const EventIsClosedForAttendes = () => {
                 <Card
                     header={
                         <>
-              Anmälan är <HighlightText>stängd</HighlightText>
+                            Anmälan är <HighlightText>stängd</HighlightText>
                         </>
                     }
                 >
-          Ni är anmälda till träffen men det går inte längre att amäla sig till
-          träffen. Nedan finns information om hur ni betalar för träffen, om ni
-          ännu inte gjort det.
+                    Ni är anmälda till träffen men det går inte längre att amäla sig till
+                    träffen. Nedan finns information om hur ni betalar för träffen, om ni
+                    ännu inte gjort det.
                 </Card>
             </div>
         </div>
@@ -165,19 +192,19 @@ const EventIsClosedForAttendes = () => {
 };
 
 type AttendingToCocktailMeetProps = {
-  eventTitle: string;
-  username?: string | null;
+    eventTitle: string;
+    username?: string | null;
 };
 
 const Attending = ({
     event,
     session,
 }: {
-  event: NopEvent;
-  session: Session | null;
-    }) => {
+    event: NopEvent;
+    session: Session | null;
+}) => {
     if (event.options.ticketUrl) {
-        return <AttendingAndPayWithSodality ticketUrl={event.options.ticketUrl}/>;
+        return <AttendingAndPayWithSodality ticketUrl={event.options.ticketUrl} />;
     }
     if (event.options.customSignupPage) {
         return <AttendingToSkargardsParty />;
@@ -201,32 +228,32 @@ const AttendingToSkargardsParty = () => {
                         <HighlightText>Er anmälan är registrerad</HighlightText>
                     </h3>
                     <div className="whitespace-pre-wrap text-lg">
-            Vad kul att ni vill hänga med på skärgårdsfest 🎉🍸🍾
+                        Vad kul att ni vill hänga med på skärgårdsfest 🎉🍸🍾
                     </div>
                     <div className="whitespace-pre-wrap text-lg">
-            Vi har nu registrerat er anmälan att ni vill vara med. Vi
-            eftersträvar att få en bra blandning på paren, så att alla ska känna
-            sig trygga och bekväma. Det gör att det kan dröja innan vi bekräftar
-            er plats. From söndagen 29/9 kommer vi att börja bjuda in par som
-            anmält sitt intresse.
+                        Vi har nu registrerat er anmälan att ni vill vara med. Vi
+                        eftersträvar att få en bra blandning på paren, så att alla ska känna
+                        sig trygga och bekväma. Det gör att det kan dröja innan vi bekräftar
+                        er plats. From söndagen 29/9 kommer vi att börja bjuda in par som
+                        anmält sitt intresse.
                     </div>
                     <div className="whitespace-pre-wrap text-lg">
-            Vi kommer att använda den email som ni använder för att logga in på
-            Night of Passion för att skicka en ev inbjudan. Där finns även
-            instruktioner för hur festavgiften om 3500kr ska betalas.
+                        Vi kommer att använda den email som ni använder för att logga in på
+                        Night of Passion för att skicka en ev inbjudan. Där finns även
+                        instruktioner för hur festavgiften om 3500kr ska betalas.
                     </div>
                     <div className="whitespace-pre-wrap text-lg">
-            Har ni frågor eller funderingar så kan ni skicka ett mail till{" "}
+                        Har ni frågor eller funderingar så kan ni skicka ett mail till{" "}
                         <a
                             className="text-[hsl(280,100%,70%)]"
                             href="mailto:fest@nightofpassion.se"
                         >
-              fest@nightofpassion.se
+                            fest@nightofpassion.se
                         </a>
                     </div>
 
                     <div className="whitespace-pre-wrap text-lg">
-            Kram på er så länge 😘
+                        Kram på er så länge 😘
                     </div>
                 </div>
             </div>
@@ -252,18 +279,18 @@ const AttendingToCocktailMeet = ({
                 <Card
                     header={
                         <>
-              Välkommen på <HighlightText>Cocktailträff 🎉🍸🍾</HighlightText>
+                            Välkommen på <HighlightText>Cocktailträff 🎉🍸🍾</HighlightText>
                         </>
                     }
                 >
                     <div className="text-lg ">
-            Nu är ni anmälda och nedan finns information hur ni betalar för
-            träffen. Efter betalningen så kommer vi lägga till er till träffen
-            och ni kan då se vilka andra som har anmält sig.
+                        Nu är ni anmälda och nedan finns information hur ni betalar för
+                        träffen. Efter betalningen så kommer vi lägga till er till träffen
+                        och ni kan då se vilka andra som har anmält sig.
                     </div>
                     <div className="text-lg ">
-            Vi kommer att maila ut mer info några dagar innan träffen. Då
-            berättar vi vilket ställe vi ska ses på. Håll utkik i er mailkorg.
+                        Vi kommer att maila ut mer info några dagar innan träffen. Då
+                        berättar vi vilket ställe vi ska ses på. Håll utkik i er mailkorg.
                     </div>
                 </Card>
                 <Card
@@ -274,18 +301,18 @@ const AttendingToCocktailMeet = ({
                     }
                 >
                     <div className="whitespace-pre-wrap text-lg">
-            Kostnaden för träffen är 100:- som ni swishar till 0700066099. Märk
-            er betalning med ert användarnamn ex &quot;passion-couple&quot;.
+                        Kostnaden för träffen är 100:- som ni swishar till 0700066099. Märk
+                        er betalning med ert användarnamn ex &quot;passion-couple&quot;.
                     </div>
                     <div>
-            Är ni på samma enhet som ni har Swish-appen installerad kan ni
-            klicka på knappen nedan för att betala.
+                        Är ni på samma enhet som ni har Swish-appen installerad kan ni
+                        klicka på knappen nedan för att betala.
                     </div>
                     <div className="flex items-center justify-center">
                         <CocktailSwishButton message={swishMessage} />
                     </div>
                     <div>
-            Eller så öppnar ni upp er swish app och skannar QR koden nedan.
+                        Eller så öppnar ni upp er swish app och skannar QR koden nedan.
                     </div>
                     <div className="p-2">
                         <SwishQR />
