a3261f3f19eb86cc5b729baf846533e93be1275b
nopops <nopops@nightofpassion.se>
2026-03-12 23:03:59 +0100

    proxy must be properly typed

--- src/proxy.js
+++
@@ -1,7 +0,0 @@
-import nextAuthMiddleware from "next-auth/middleware";
-
-export function proxy(request) {
-    return nextAuthMiddleware(request);
-}
-
-export const config = { matcher: ["/app/:path*"] };
